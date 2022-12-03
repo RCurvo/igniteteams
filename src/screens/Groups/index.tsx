e5a98f5ce1a6edae26799a26746008c4395011bc
RCurvo <renan.curvo@gmail.com>
2022-12-03 11:32:04 -0300

    feat: add theme and types

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { Container, Title } from './styles';
 
 export function Groups() {
@@ -8,12 +7,3 @@ export function Groups() {
         </Container>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
