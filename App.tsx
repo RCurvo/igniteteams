3669610d9b75246d4f48934759be2bc068d56935
RCurvo <renan.curvo@gmail.com>
2022-12-03 12:23:12 -0300

    style: StatusBar

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { Groups } from '@screens/Groups';
-import theme from './src/theme';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import theme from './src/theme';
+import { StatusBar } from 'react-native';
+
 import { Loading } from '@components/Loading';
+import { Groups } from '@screens/Groups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor='transparent'
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
