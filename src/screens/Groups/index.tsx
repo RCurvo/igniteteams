c7fb11b9f7bef0e5656a16ffb53d7616028b16ef
RCurvo <renan.curvo@gmail.com>
2022-12-03 11:01:52 -0300

    First commit Groups with styled components

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Container, Title } from './styles';
+
+export function Groups() {
+    return (
+        <Container>
+            <Title>Groups</Title>
+        </Container>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
