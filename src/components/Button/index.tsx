5915eaa7be21e3d968bf783c2511edc4ca0ac9ec
RCurvo <renan.curvo@gmail.com>
2022-12-05 10:39:54 -0300

    feat: Add Button Component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Title, ButtonTypeStyleProps } from "./styles";
+import { TouchableOpacityProps } from "react-native";
+
+type ButtonProps = TouchableOpacityProps & {
+    title: string;
+    type?: ButtonTypeStyleProps
+}
+
+export function Button({ title, type = 'PRIMARY', ...rest }: ButtonProps) {
+    return (
+        <Container
+            type={type}
+            {...rest}>
+            <Title>
+                {title}
+            </Title>
+        </Container>
+    )
+}
