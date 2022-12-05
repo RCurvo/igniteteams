eda6a0cd9ef7452085f077d2873cf51194d5a24a
RCurvo <renan.curvo@gmail.com>
2022-12-05 09:24:12 -0300

    Feat: Add Flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
+import { FlatList } from 'react-native'
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 
 export function Groups() {
+    const [groups, setGroups] = useState<string[]>(['Rocketseat']);
+
     return (
         <Container>
             <Header />
             <Highlight
                 title='Turmas'
-                subtitle='Jogue com a sua turma' />
-            <GroupCard title="Galera do Ignite" />
+                subtitle='Jogue com a sua turma'
+            />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+            />
         </Container>
     );
 }
