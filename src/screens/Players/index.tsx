fdf6f767a5303a39c9d1194c2c8307d1417a139a
RCurvo <renan.curvo@gmail.com>
2022-12-06 11:30:33 -0300

    feat: Teams Header

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ButtonIcon } from '@components/ButtonIcon'
-import { Container, Form } from './styles'
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 import { Input } from '@components/Input'
 import { Filter } from '@components/Filter'
+import { FlatList } from 'react-native'
+import { useState } from 'react'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players, setPlayers] = useState(['Renan', 'Bruna', 'Roberto'])
+
   return (
     <Container>
       <Header showBackButton />
@@ -17,8 +22,21 @@ export function Players() {
         <Input placeholder="Nome do jogador" autoCorrect={false} />
         <ButtonIcon icon="add" type="PRIMARY" />
       </Form>
-      <Filter title="Time A" isActive={true} />
-      <Filter title="Time B" />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumberOfPlayers> {players.length}</NumberOfPlayers>
+      </HeaderList>
     </Container>
   )
 }
