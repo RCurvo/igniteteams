8cdda268fbdd05acb4cbcb48c08c6fb99c828f15
RCurvo <renan.curvo@gmail.com>
2022-12-06 19:59:01 -0300

    feat: use internal storage

---
+++ src/storage/group/groupsGetAll.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { GROUP_COLLECTION } from '@storage/storageConfig'
+import { G } from 'react-native-svg'
+
+export async function groupsGetAll() {
+  try {
+    const storage = await AsyncStorage.getItem(GROUP_COLLECTION)
+
+    const groups: string[] = storage ? JSON.parse(storage) : []
+    return groups
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
