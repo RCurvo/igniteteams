bffd643c326541822cdd414aa74bf6b427c264a4
RCurvo <renan.curvo@gmail.com>
2022-12-07 13:17:16 -0300

    feat: handle group navigation and add exceptions

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { useState, useCallback } from 'react'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import { FlatList } from 'react-native'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
@@ -7,6 +7,7 @@ import { GroupCard } from '@components/GroupCard'
 import { ListEmpty } from '@components/ListEmpty'
 import { Button } from '@components/Button'
 import { Container } from './styles'
+import { groupsGetAll } from '@storage/group/groupsGetAll'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -17,6 +18,26 @@ export function Groups() {
     navigation.navigate('new')
   }
 
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log('use Effect executou')
+      fetchGroups()
+    }, []),
+  )
+
   return (
     <Container>
       <Header />
@@ -24,7 +45,9 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard onPress={() => handleOpenGroup(item)} title={item} />
+        )}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => (
           <ListEmpty message="Nenhuma turma foi cadastrada. Que tal cadastrar a primeira turma?" />
