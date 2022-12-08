687dd953dcac9c45f160c48a1be4f36f404e3136
RCurvo <renan.curvo@gmail.com>
2022-12-08 16:20:22 -0300

    feat: Add playerRemove functionality

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -14,6 +14,7 @@ import { AppError } from '@utils/AppError'
 import { playerAddByGroup } from '@storage/player/playerAddByGroup'
 import { playersGetByGroupAndTeam } from '@storage/player/playerGetByGroupAndTeam'
 import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO'
+import { playerRemoveByGroup } from '@storage/player/playerRemoveByGroup'
 
 type RouteParams = {
   group: string
@@ -68,6 +69,16 @@ export function Players() {
     }
   }
 
+  async function handlePlayerRemove(playerName: string) {
+    try {
+      await playerRemoveByGroup(playerName, group)
+      fetchPlayersByTeam()
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Remover pessoa', 'Não foi possível remover essa pessoa')
+    }
+  }
+
   useEffect(() => {
     console.log('UseEffect Team executou')
     fetchPlayersByTeam()
@@ -109,7 +120,12 @@ export function Players() {
         data={players}
         keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <PlayerCard name={item.name} onRemove={() => {}} />
+          <PlayerCard
+            name={item.name}
+            onRemove={() => {
+              handlePlayerRemove(item.name)
+            }}
+          />
         )}
         ListEmptyComponent={() => (
           <ListEmpty message="Não há jogadores nesse time" />
---
+++ src/storage/player/playerRemoveByGroup.ts
@@ -0,0 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { PLAYERS_COLLECTION } from '@storage/storageConfig'
+import { playersGetByGroup } from './playersGetByGroup'
+
+export async function playerRemoveByGroup(playerName: string, group: string) {
+  try {
+    const storage = await playersGetByGroup(group)
+    const filterd = storage.filter((player) => player.name !== playerName)
+    const players = JSON.stringify(filterd)
+
+    await AsyncStorage.setItem(`${PLAYERS_COLLECTION}-${group}`, players)
+  } catch (error) {}
+}
